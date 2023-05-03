45ad830d4b0bcf6a8f17e31f82a54ee360b79b6b
michal97736 <useremail>
2023-05-03 16:01:00 +0300

    my commit message

---
+++ ToDoListClient/build/precache-manifest.9df332ad98a73ba97281ea1bf3514387.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f1b6d967829918ba7e0f837e27e341",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05a5fbcf5a645693e8b6",
+    "url": "/static/js/2.e8b56d4c.chunk.js"
+  },
+  {
+    "revision": "abf6fcace3adbf8ab345683d164de87d",
+    "url": "/static/js/2.e8b56d4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74cc1fb2de270aebd929",
+    "url": "/static/js/main.53107b87.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.53107b87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed24628c41b89e511d36",
+    "url": "/static/js/runtime-main.76e1faf0.js"
+  }
+]);
